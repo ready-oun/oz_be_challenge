e06025a8aca6dce567ac4134c211a822c1f2c590
ready-oun <heeyn.lim@gmail.com>
2024-01-08 11:40:59 +0900

    update user-agent dict

--- 5. Web Scraping/naver_1.py
+++ 5. Web Scraping/naver_1.py
@@ -1,6 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
+# 변수 정해서 유저정보명 담기 - 딕트 형태로 
+header_user = {
+    "User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+}
+# user-agent 정보로 사람인 척 하는 방법 
+# Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36
+
 url = "https://naver.com"
 
 #get 방식 : 서버에게 리소스(자원)를 요청, 데이터를 수신하는 기능 
