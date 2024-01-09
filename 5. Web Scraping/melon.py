c84789b1802b51b1c8c78cd1cedbae2fb2b2dde8
ready-oun <heeyn.lim@gmail.com>
2024-01-09 11:55:24 +0900

    update singer & album ID value function

--- 5. Web Scraping/melon.py
+++ 5. Web Scraping/melon.py
@@ -10,28 +10,19 @@ req = requests.get(url, headers=header_user)
 html = req.text
 soup = BeautifulSoup(html, "html.parser")
 
-# class는 점(.)으로 구분한다. 
+# class는 점(.)으로 구분한다.
+ 
 lst50 = soup.select(".lst50")
+lst100 = soup.select(".lst100")
 
-for rank, i in enumerate(lst50, 1):
+lst_all = lst50 + lst100
+
+for rank, i in enumerate(lst_all, 1):
     title = i.select_one(".ellipsis.rank01 a") # 줄바꿈 없애기 - select_one().a 해서 a태그 선택? 
     singer = i.select_one(".checkEllipsis a")
     album = i.select_one(".ellipsis.rank03 a")
     
     # print(f'제목: {title.text.strip()}') # strip()으로 span - a 태그의 중복을 제거할 수 있는 라이브러리 
-    print(f'순위: {rank}')
-    print(f'제목: {title.text}')
-    print(f'가수: {singer.text}')
-    print(f'앨범: {album.text}')
-    print()
-
-lst100 = soup.select(".lst100")
-
-for rank, i in enumerate(lst100, 1):
-    title = i.select_one(".ellipsis.rank01 a") # 줄바꿈 없애기 - select_one().a 해서 a태그 선택? 
-    singer = i.select_one(".checkEllipsis a")
-    album = i.select_one(".ellipsis.rank03 a")
-
     print(f'순위: {rank}')
     print(f'제목: {title.text}')
     print(f'가수: {singer.text}')
