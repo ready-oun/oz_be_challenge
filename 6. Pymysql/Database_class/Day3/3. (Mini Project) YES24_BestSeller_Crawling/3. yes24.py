0e7e38c86f22fe421ed79be083ba23d307908d6e
ready-oun <heeyn.lim@gmail.com>
2024-01-17 05:29:35 +0900

    update crawling settings of yes24

--- 6. Pymysql/Database_class/Day3/3. (Mini Project) YES24_BestSeller_Crawling/3. yes24.py
+++
@@ -1,9 +0,0 @@
-# robin@imhuiyeon-ui-MacBookPro Day3 % cd /Users/robin/Desktop/oz_be_challenge/6.Pymysql/Day3
-
-# cd: no such file or directory: /Users/robin/Desktop/oz_be_challenge/6.Pymysql/Day3
-# robin@imhuiyeon-ui-MacBookPro Day3 % python3 -m venv .venv
-
-# robin@imhuiyeon-ui-MacBookPro Day3 % source .venv/bin/activate
-
-# (.venv) robin@imhuiyeon-ui-MacBookPro Day3 % 
-
