171de3163bc8ed44ce6bd0b972ed2d26b594e611
ready-oun <heeyn.lim@gmail.com>
2024-01-24 19:05:29 +0900

    update rendering

--- 7.Flask/env_crawling/app.py
+++ 7.Flask/env_crawling/app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
+import pymysql
 
 app = Flask(__name__)
 
+db = pymysql.connect (
+	host = '127.0.0.1',
+    user = 'root',
+    password = 'robin_robin',
+    db='kream',
+    charset='utf8mb4'
+)
+
+cur = db.cursor()
+sql = "SELECT * FROM kream"
+cur.execute(sql)
+
+kream_data = cur.fetchall()
+
 @app.route('/')
 def index():
-	return 'Hello, Flask!'
+	return render_template('index.html', data_list = kream_data)
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.debug = True
+	app.run
