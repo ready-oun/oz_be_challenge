b349f25f0214055f465bc05521ec576f9a6b7630
ready-oun <heeyn.lim@gmail.com>
2024-01-25 19:51:04 +0900

    add a feature of filtering categories

--- 7.Flask/env_crawling/app.py
+++ 7.Flask/env_crawling/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import pymysql
 
 app = Flask(__name__)
@@ -8,19 +8,27 @@ db = pymysql.connect (
     user = 'root',
     password = 'robin_robin',
     db='KREAM',
-    charset='utf8mb4'
+    charset='utf8mb4',
+    cursorclass = pymysql.cursors.DictCursor
 )
 
 cur = db.cursor()
-sql = "SELECT * FROM product"
-cur.execute(sql)
 
-kream_data = cur.fetchall()
+@app.route('/', methods=('GET',))
+def index():    
+    category = request.args.get('category_name')
+    if category:
+        sql = f"SELECT * FROM product WHERE category_name = '{category}'"
+    else:
+        sql= f"SELECT * FROM product"
+    cur.execute(sql)
 
-@app.route('/')
-def index():
-	return render_template('index.html', data_list = kream_data)
-
+    kream_data = cur.fetchall()
+    # print(kream_data[0])
+    kream_data_len = len(kream_data)
+    
+    return render_template('index.html', kream_data = kream_data, kream_data_len=kream_data_len)
+# (1) 검색어, (2) 페이지네이션 !!!! 
 if __name__ == '__main__':
 	app.debug = True
 	app.run()
